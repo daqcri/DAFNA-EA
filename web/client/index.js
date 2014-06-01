1e87b2cf1ac3a644c811cae62760cb5b8558d11e
szahbia <s.zahbia@gmai.com>
2014-06-01 09:53:36 +0300

    Updated

--- web/client/index.js
+++ web/client/index.js
@@ -1,54 +1,56 @@
+//Json coding for creating algorithm and their parameters
+
 var algorithms = [
   {name: "General Parameters", params: [
-    {name: 'cosineSimDiffStoppingCriteria', value: 0.001, dataType: 'double', min: 0, max: 1.0},
-    {name: 'startingTrust', value: 0.8, dataType: 'double' , min: 0, max: 1.0},
-    {name: 'startingErrorFactor', value: 0.4, dataType: 'double' , min: 0, max: 1.0},
-    {name: 'startingConfidence', value: 1, dataType: 'double', min: 0, max: 1.0},
+    {name: 'cosineSimDiffStoppingCriteria', value: 0.001, dataType: 'double', min: 0, max: 1.0, desc: 'put description here'},
+    {name: 'startingTrust', value: 0.8, dataType: 'double' , min: 0, max: 1.0, desc: 'put description here'},
+    {name: 'startingErrorFactor', value: 0.4, dataType: 'double' , min: 0, max: 1.0, desc: 'put description here'},
+    {name: 'startingConfidence', value: 1, dataType: 'double', min: 0, max: 1.0, desc: 'put description here'},
   ]},
   {name: "Cosine", params: [
-    {name: 'ampeningFactorCosine', value: 0.2, dataType: 'double', min: 0, max: 1},
+    {name: 'ampeningFactorCosine', value: 0.2, dataType: 'double', min: 0, max: 1, desc: 'put description here'},
   ]},
   {name: "2-Estimates", params: [
-    {name: 'normalizationWeight', value: 0.5, dataType: 'double', min: 0, max: 1},
+    {name: 'normalizationWeight', value: 0.5, dataType: 'double', min: 0, max: 1, desc: 'put description here'},
   ]},
   {name: "3-Estimates", params: [
-    {name: 'normalizationWeight', value: 0.5, dataType: 'double', min: 0, max: 1},
+    {name: 'normalizationWeight', value: 0.5, dataType: 'double', min: 0, max: 1, desc: 'put description here'},
   ]},
   {name: "Depen", params: [
-    {name: 'alpha', value: 0.2, dataType: 'double', min: 0, max: 0.5},
-    {name: 'c', value: 0, dataType: 'double', min: 0, max: 1},
-    {name: 'n', value: 100, dataType: 'int', min: 1},
+    {name: 'alpha', value: 0.2, dataType: 'double', min: 0, max: 0.5, desc: 'put description here'},
+    {name: 'c', value: 0, dataType: 'double', min: 0, max: 1, desc: 'put description here'},
+    {name: 'n', value: 100, dataType: 'int', min: 1, desc: 'put description here'},
   ]},
   {name: "Accu", params: [
-    {name: 'alpha', value: 0.2, dataType: 'double', min: 0, max: 0.5},
-    {name: 'c', value: 0, dataType: 'double', min: 0, max: 1},
-    {name: 'n', value: 100, dataType: 'int', min: 1},
+    {name: 'alpha', value: 0.2, dataType: 'double', min: 0, max: 0.5, desc: 'put description here'},
+    {name: 'c', value: 0, dataType: 'double', min: 0, max: 1, desc: 'put description here'},
+    {name: 'n', value: 100, dataType: 'int', min: 1, desc: 'put description here'},
   ]},
   {name: "AccuSim", params: [
-    {name: 'alpha', value: 0.2, dataType: 'double', min: 0, max: 0.5},
-    {name: 'c', value: 0, dataType: 'double', min: 0, max: 1},
-    {name: 'n', value: 100, dataType: 'int', min: 1},
+    {name: 'alpha', value: 0.2, dataType: 'double', min: 0, max: 0.5, desc: 'put description here'},
+    {name: 'c', value: 0, dataType: 'double', min: 0, max: 1, desc: 'put description here'},
+    {name: 'n', value: 100, dataType: 'int', min: 1, desc: 'put description here'},
   ]},
   {name: "AccuNoDep", params: [
-    {name: 'alpha', value: 0.2, dataType: 'double', min: 0, max: 0.5},
-    {name: 'c', value: 0, dataType: 'double', min: 0, max: 1},
-    {name: 'n', value: 100, dataType: 'int', min: 1},
+    {name: 'alpha', value: 0.2, dataType: 'double', min: 0, max: 0.5, desc: 'put description here'},
+    {name: 'c', value: 0, dataType: 'double', min: 0, max: 1, desc: 'put description here'},
+    {name: 'n', value: 100, dataType: 'int', min: 1, desc: 'put description here'},
   ]},
   {name: "TruthFinder", params: [
-    {name: 'similarityConstant', value: 0.5, dataType: 'double', min: 0, max: 1},
+    {name: 'similarityConstant', value: 0.5, dataType: 'double', min: 0, max: 1, desc: 'put description here'},
   ]},
   {name: "Simple LCA and Guess LCA", params: [
-    {name: 'Bita1', value: 0.5, dataType: 'double', min: 0, max: 1},
+    {name: 'Bita1', value: 0.5, dataType: 'double', min: 0, max: 1, desc: 'put description here'},
   ]},
   {name: "MLE", params: [
-    {name: 'Bita1', value: 0.5, dataType: 'double', min: 0, max: 1},
-    {name: 'r', value: 0.5, dataType: 'double', min: 0, max: 1},
+    {name: 'Bita1', value: 0.5, dataType: 'double', min: 0, max: 1, desc: 'put description here'},
+    {name: 'r', value: 0.5, dataType: 'double', min: 0, max: 1, desc: 'put description here'},
   ]},
 ]
 
 $(function() {
   // create accordion algorithm panes
-  var container = $("#input_claims_pane")
+  var container = $("#tab_pane")
   var html = ""
   $.each(algorithms, function(index, algorithm) {
     
@@ -58,13 +60,13 @@ $(function() {
     $.each(algorithm.params, function(pindex, param){
       
 
-      params += "<tr><td>" + param.name + " </td><td><input class='spinner' min='"+param.min+"' max='"+param.max+"' value='"+param.value+"'></input></td></tr>" 
+      params += "<tr><td width='100%'><span>" + param.name + "</span><span class='ui-icon ui-icon-info' title='"+param.desc+"'></span> </td><td><input class='spinner' min='"+param.min+"' max='"+param.max+"' value='"+param.value+"'></input></td></tr>" 
       
     });
     html += "<h3>"+algorithm.name+"</h3><div><table cellpadding=0 cellspacing=0 width='100%' border=0 style ='border-collapse:collapse;'>" + params+ "</table></div>"
   });
   
-  container.after(html)
+  container.append(html)
   $( ".spinner" ).spinner({
       step: 0.1,
       numberFormat: "n",
@@ -73,21 +75,30 @@ $(function() {
       $(this).spinner('option', 'max', parseFloat($(this).attr("max")));
     });
 
+  // Algorithm section in tab-1 under 3rd header
+   $(function(){
+     var container = $("#algo_pane")
+     var html =""
+     
+     $.each(algorithms, function(index, algorithm){
 
+       html   += "<label><input type='checkbox'/>"+ algorithm.name +"</label>"
 
-
-
-
+   });
+   container.append(html);
+   });
   // create the accordion
-    $( "#accordion" ).accordion({
+    $( ".accordion" ).accordion({
       collapsible: true,
       active:false,
-      
+      heightStyle: "content",
     });
  }); 
 
+//Check Checkbox selected
+ 
 
-
+//Tree creation 
 $(function () {
     // 6 create an instance when the DOM is ready
     $('.jstree')
@@ -106,11 +117,11 @@ $(function () {
   });
 
 
-
+// Jquery UI layout 
 $(function(){
    $('body').layout({
     applyDefaultStyles: true,
-    west__size: "40%",
+    west__size: "35%",
     west__minSize: "10%",
     west__maxSize: "50%",
     west__onresize: function() {
@@ -124,7 +135,7 @@ $(function(){
 });
 
 
-
+// Jquery UI nested layout
 $(function(){
 $('.inner').layout({
      applyDefaultStyles: true,
@@ -137,22 +148,27 @@ $('.inner').layout({
 });
 });
 
+// File uplaoding 
     $(function () {
- $('#upload_file').fileupload({
-        url: url,
-        
+      $('.fileupload').fileupload({
         dataType: 'json',
         done: function (e, data) {
-          //  $.each(data.result.files, function (index, file) {
-              //  $('<p/>').text(file.name).appendTo('#files');
-              alert("File uploaded");
-          //  });
-        }, 
-        fail: function(e, data){
-              alert("Failed");
-
+         // console.log(data)
+          alert(data.status);
+        },
+        progressall: function (e, data) {
+            console.log(data)
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+             $(this).parent().next().find('.progress-bar').css(
+                'width',
+                progress + '%'
+            );
         }
 
-}); 
-});
+      }); 
+    });
 
+// Creating Tab
+$(function() {
+    $( "#tabs" ).tabs();
+});
